2d08ae9725769781e685d267fe47b7f8818b8137
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 16:49:45 +0530

    Method calling for UC3

--- Program.cs
+++ Program.cs
@@ -13,17 +13,18 @@
             obj.push(56);
             Console.WriteLine();*/
 
-            
-            PeakandPop obj = new PeakandPop();
-            Console.WriteLine("After push the elements are : ");
 
-            obj.push(70);
-            obj.push(30);
-            obj.push(56);
+            //PeakandPop obj = new PeakandPop();
+            CreateQueue obj = new CreateQueue();
+            Console.WriteLine("After Enqueue the elements are : ");
+
+            obj.enqueue(70);
+            obj.enqueue(30);
+            obj.enqueue(56);
             Console.WriteLine("-------------------");
 
-            Console.WriteLine("Item popped from stack = " + obj.pop());
-            Console.WriteLine("Peek Element of the stack is : " + obj.peek());
+            //Console.WriteLine("Item popped from Queue = " + obj.dequeue());
+            Console.WriteLine("Peek Element of the Queue is : " + obj.peek()); ;
         }
     }
 }
