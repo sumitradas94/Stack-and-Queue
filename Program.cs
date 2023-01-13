e2870f1d6184c7e796bfa4abdafc8c3747c7d131
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 16:44:25 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -4,16 +4,26 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data structure Problem");
-            CreateStack obj = new CreateStack();
+           Console.WriteLine("Welcome to Data structure Problem");
+            /*CreateStack obj = new CreateStack();
             Console.WriteLine("After push the elements are :- ");
 
             obj.push(70);
             obj.push(30);
             obj.push(56);
-            Console.WriteLine();
+            Console.WriteLine();*/
 
-            Console.WriteLine("Top Element of the stack is : " + obj.peek());
+            
+            PeakandPop obj = new PeakandPop();
+            Console.WriteLine("After push the elements are : ");
+
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Item popped from stack = " + obj.pop());
+            Console.WriteLine("Peek Element of the stack is : " + obj.peek());
         }
     }
 }
