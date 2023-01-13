f1e3db529503d80daec48a3088e58e76d0e53885
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 16:43:57 +0530

    Peak and pop Element

---
+++ PeakandPop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    public class PeakandPop
+    {
+        Node head;
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                head = node;
+                node.next = temp;
+            }
+            Console.WriteLine("Item pushed into stack -> " + data);
+
+        }
+
+        public int pop()
+        {
+            int popped;
+            if (head == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return 0;
+            }
+            else
+            {
+                popped = head.data;
+                head = head.next;
+                return popped;
+            }
+        }
+
+        public int peek()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return 0;
+            }
+            else
+            {
+                return head.data;
+            }
+        }
+    }
+}
