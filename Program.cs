523ef88da17e6c2708efb05445d7277edcec2b06
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 16:35:00 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data structure Problem");
+            CreateStack obj = new CreateStack();
+            Console.WriteLine("After push the elements are :- ");
+
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            Console.WriteLine();
+
+            Console.WriteLine("Top Element of the stack is : " + obj.peek());
         }
     }
 }
