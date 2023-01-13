d71082db34c2357b1728314ebc3a034bc73a3446
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 16:58:39 +0530

    modifying for UC4

--- Program.cs
+++ Program.cs
@@ -14,8 +14,9 @@
             Console.WriteLine();*/
 
 
-            //PeakandPop obj = new PeakandPop();
-            CreateQueue obj = new CreateQueue();
+            
+
+            AbilityDequeue obj = new AbilityDequeue();
             Console.WriteLine("After Enqueue the elements are : ");
 
             obj.enqueue(70);
@@ -23,8 +24,10 @@
             obj.enqueue(56);
             Console.WriteLine("-------------------");
 
-            //Console.WriteLine("Item popped from Queue = " + obj.dequeue());
-            Console.WriteLine("Peek Element of the Queue is : " + obj.peek()); ;
+            Console.WriteLine("Elements popped from Queue = " + obj.dequeue());
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Peek Element of the Queue is : " + obj.peek());
+
         }
     }
 }
